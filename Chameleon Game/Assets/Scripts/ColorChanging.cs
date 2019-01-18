3499b7502f45333b045f30418af5411b48f3a4d2
Kelly Herstine <kelly.herstine@mymail.champlain.edu>
2019-01-17 20:02:02 -0500

    Added back in the files that were deleted yesterday

---
+++ Chameleon Game/Assets/Scripts/ColorChanging.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChanging : MonoBehaviour {
+    Renderer playerMat;
+	// Use this for initialization
+	void Start () {
+        playerMat = GetComponent<Renderer>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionStay(Collision col)
+    {
+        if(Input.GetKey(KeyCode.C))
+        {
+            playerMat.material = col.gameObject.GetComponent<Renderer>().material;
+        }
+    }
+   
+}
---
+++ Chameleon Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float speed = 5.0f;
+    [SerializeField] float rotationSpeed = 200.0f;
+
+    Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(0, Input.GetAxis("Horizontal") * Time.deltaTime * rotationSpeed, 0);
+        transform.Translate(0, 0, Input.GetAxis("Vertical") * Time.deltaTime * speed);
+    }
+}
